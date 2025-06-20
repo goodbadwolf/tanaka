0d2db401a2f432c61d7f7917b61ffe1cf8c79b17
Manish Mathai <manishpmathai@gmail.com>
2025-06-20 16:50:21 -0700

    refactor: clean up code formatting and remove unused imports
    - Remove unused import from watch.ts
    - Add underscore to unused promise parameter in unhandledRejection handler
    - Simplify return statement in build.ts
    - Minor code formatting improvements

--- extension/scripts/watch.ts
+++ extension/scripts/watch.ts
@@ -1,17 +1,10 @@
 #!/usr/bin/env -S npx tsx
-import {
-  logger,
-  setupProcessHandlers,
-  runInitialBuild,
-  runCLI,
-} from './common.js';
+import { logger, setupProcessHandlers, runCLI } from './common.js';
 import { ProcessManager } from './process-utils.js';
 import { viteWatchConfig } from './vite-utils.js';
 
 async function watch(): Promise<void> {
   logger.info('Starting watch mode...');
-
-  await runInitialBuild();
 
   const pm = new ProcessManager();
   pm.spawn(viteWatchConfig());
