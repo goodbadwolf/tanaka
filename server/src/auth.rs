7ed08bd45ddbe8eca2e19336a0becc82094737ac
Manish Mathai <manishpmathai@gmail.com>
2025-06-20 18:00:51 -0700

    test: testing pre-commit hook

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -1,6 +1,6 @@
 use axum::{extract::Request, http::StatusCode, middleware::Next, response::Response};
 
-const BEARER_TOKEN: &str = "tanaka-secret-token";
+const BEARER_TOKEN: &str = "tanaka-secret-token"; // Test comment
 
 pub async fn auth_middleware(req: Request, next: Next) -> Result<Response, StatusCode> {
     let auth_header = req
