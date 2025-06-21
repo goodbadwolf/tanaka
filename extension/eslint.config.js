d9bb04c744208d23dd0701e81675465433f8c229
Manish Mathai <manishpmathai@gmail.com>
2025-06-20 17:37:48 -0700

    fix: upgrade ESLint to v9 and migrate to flat config
    - Upgrade ESLint from deprecated v8 to v9
    - Migrate from .eslintrc.json to eslint.config.js flat config
    - Update web-ext to v8.8.0
    - Add typescript-eslint package for better v9 support
    - Remove deprecation warnings

---
+++ extension/eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '*.config.js'],
+  }
+);
